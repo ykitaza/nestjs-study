972dd96cb1de48151827a184be6132cf6bc08ee7
Yuki Kitazawa <yuki.kitazawa.unicast@gmail.com>
2023-01-01 16:24:02 +0900

    $ nest g module todo

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
