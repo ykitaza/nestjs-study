b57ef61a1c9e3a69ee2192297eea6c64df5c752c
Yuki Kitazawa <yuki.kitazawa.unicast@gmail.com>
2023-01-02 15:46:15 +0900

    prisma service の実装

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// dto クラスバリデーションに使用
+import { ValidationPipe } from '@nestjs/common';
+import { Request } from 'express';
+// クッキーの処理に使用
+import * as cookieParser from 'cookie-parser';
+// csrf対策
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
+  app.enableCors({
+    // JWTをcookieベースでやり取りするための設定
+    credentials: true,
+    // CORSを許可するorigin(スキーマ、ドメイン、ポート)を指定
+    origin: ['http://localhost:3000'],
+  })
   await app.listen(3005);
 }
 bootstrap();
--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
 @Module({
-  providers: [PrismaService]
+  providers: [PrismaService],
+  // prismaServiceを別モジュールで使用できるようにexportsする
+  exports: [PrismaService]
 })
-export class PrismaModule {}
+export class PrismaModule { }
