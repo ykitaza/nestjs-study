4fa87835d425d0371590d13e909481afa4bbbaed
Yuki Kitazawa <yuki.kitazawa.unicast@gmail.com>
2023-01-02 22:09:57 +0900

    login logoutをルーティング

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -21,4 +21,42 @@ export class AuthController {
     signUp(@Body() dto: AuthDto): Promise<Msg> {
         return this.authService.signUp(dto)
     }
+
+    // status200を返すためのデコレータ（デフォルトでは全て201が返る。）
+    @HttpCode(HttpStatus.OK)
+    @Post('login')
+    async login(
+        @Body() dto: AuthDto,
+        // passthroght: trueにすると、expressのres.cookieと、nestjsの「オブジェクトをJOSNにシリアライズする機能」を両立できる。
+        @Res({ passthrough: true }) res: Response
+    ): Promise<Msg> {
+        const jwt = await this.authService.login(dto);
+        res.cookie('access_token', jwt.accessToken, {
+            httpOnly: true,
+            secure: false,
+            sameSite: 'none',
+            path: "/"
+        })
+        return {
+            message: 'success',
+        }
+    }
+
+    @HttpCode(HttpStatus.OK)
+    @Post('logout')
+    logout(
+        @Req() req: Request,
+        @Res({ passthrough: true }) res: Response
+    ): Msg {
+        // logoutAPIはaccess_tokenを空にする。
+        res.cookie('access_token', '', {
+            httpOnly: true,
+            secure: false,
+            sameSite: 'none',
+            path: "/"
+        })
+        return {
+            message: 'ok',
+        }
+    }
 }
