677c23db7c30d2034dfcdb2656f89e4094aa4ea2
Yuki Kitazawa <yuki.kitazawa.unicast@gmail.com>
2023-01-03 20:21:47 +0900

    prismaServiceを修正

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,16 +1,34 @@
-import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config'
+// import { Injectable } from '@nestjs/common';
+// import { ConfigService } from '@nestjs/config'
+// import { PrismaClient } from '@prisma/client';
+
+// @Injectable()
+// export class PrismaService extends PrismaClient {
+//     constructor(private readonly config: ConfigService) {
+//         super({
+//             datasources: {
+//                 db: {
+//                     url: config.get('DATABASE_URL')
+//                 }
+//             }
+//         })
+//     }
+// }
+
+
+// prisma との連携公式ドキュメント: https://docs.nestjs.com/recipes/prisma
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
-    constructor(private readonly config: ConfigService) {
-        super({
-            datasources: {
-                db: {
-                    url: config.get('DATABASE_URL')
-                }
-            }
-        })
+export class PrismaService extends PrismaClient implements OnModuleInit {
+    async onModuleInit() {
+        await this.$connect();
+    }
+
+    async enableShutdownHooks(app: INestApplication) {
+        this.$on('beforeExit', async () => {
+            await app.close();
+        });
     }
 }
